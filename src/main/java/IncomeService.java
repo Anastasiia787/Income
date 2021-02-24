723a875f1e3d771c093958134eb2b2d30b8b4098
Anastasia <nastenaprokopeva78@gmail.com>
2021-02-24 23:43:24 +0300

    income

--- src/main/java/IncomeService.java
+++ src/main/java/IncomeService.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class IncomeService {
-}
+public class IncomeService {
+        int calculate(int sum){
+            int percent = 3;
+            int income = sum * percent / 100;
+
+            int limit = 3_000;
+            if (income > limit) {
+                income = limit;
+            }
+            return income;
+        }
+    }
+
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        IncomeService service = new IncomeService ();
+        int income = service.calculate(1_000);
+        System.out.println(income);
+    }
 }
